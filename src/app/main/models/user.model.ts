88f8f999200afcb6f29ac763db40a9ab461446fb
Younes EL Ansari <yelansari@valueit.ma>
2018-08-06 16:47:17 +0100

    Added logged user name and avatar

--- src/app/main/models/user.model.ts
+++ src/app/main/models/user.model.ts
@@ -2,11 +2,9 @@ import {User as CustomUser} from '../../core/models/user.model';
 
 export class User extends CustomUser {
   accountType: string;
-  profileType: string;
   address: string;
   admin: boolean;
   avatar: string;
-  accounts: Array<string>;
   settings: any;
   enabled: boolean;
   phone: string;
