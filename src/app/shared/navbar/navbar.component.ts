3d6cd02ee7a14fd89e3ca827ad4268649981333a
relkaddouri <relkaddouri@valueit.ma>
2019-01-09 17:40:42 +0100

    Added : New translations

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+ import {Component, OnInit} from '@angular/core';
 import {OAuthService} from '../../core/services/security/oauth.service';
 import {UtilsService} from '../../main/services/utils.service';
 import {ToastrService} from 'ngx-toastr';
